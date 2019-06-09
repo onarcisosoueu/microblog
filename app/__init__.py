17fa3c31de1abd997b71f91d8f00ed43aff8c324
André Narciso <andrenarciso6@gmail.com>
2019-06-09 19:04:42 +0200

    Added Email Support
    Added reset password feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from config import Config
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from config import Config
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
